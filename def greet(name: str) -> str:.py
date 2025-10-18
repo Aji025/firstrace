265830f936b160572d878fcafd5f1298c97bb95b
Aji025 <ajiteshsrivastava106@gmail.com>
2025-10-18 06:31:55 +0000

    Initial commit

---
+++ def greet(name: str) -> str:.py
@@ -0,0 +1,23 @@
+print("my first experiment")
+
+n= 5
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print(type(arr))
+
+
+def say_hi(self):
+        print('Hello, my name is', self)
+
+
+say_hi("ajitesh")
+
+
+def numberi(n1,n2):
+     print(n1+n2, n1*n2, n1/n2)
+
+numberi(25,26)
